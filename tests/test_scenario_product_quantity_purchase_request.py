f7105e5ee8a4a759ac2aef2d34c102b96f73fefc
Kevin Ortega Garcia <kevin.ortega@nan-tic.com>
2026-09-21 16:32:04 +0200

    Now quantity fields are being properly calculated on product change within lines that use them. Corrected tests to be standard with the usual way of implementing scenarios. Task #191457

--- tests/test_scenario_product_quantity_purchase_request.py
+++ tests/test_scenario_product_quantity_purchase_request.py
@@ -1,26 +1,33 @@
-from trytond.pool import Pool
-from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+import unittest
+
+from proteus import Model
+from trytond.tests.test_tryton import drop_db
+from trytond.tests.tools import activate_modules
 
 
-class ProductQuantityPurchaseRequestTestCase(ModuleTestCase):
-    module = 'product_quantity'
-    extras = ['purchase_request', 'stock_lot']
+class Test(unittest.TestCase):
 
-    @with_transaction()
+    def setUp(self):
+        drop_db()
+        super().setUp()
+
+    def tearDown(self):
+        drop_db()
+        super().tearDown()
+
     def test(self):
-        pool = Pool()
-        Uom = pool.get('product.uom')
-        Template = pool.get('product.template')
-        Product = pool.get('product.product')
-        unit, = Uom.search([('name', '=', 'Unit')])
-        template, = Template.create([{
-            'name': 'Product', 'default_uom': unit.id}])
-        product, = Product.create([{'template': template.id}])
-        line = pool.get('purchase.request')(product=product)
+        activate_modules(['product_quantity', 'purchase_request', 'stock_lot'])
+
+        Uom = Model.get('product.uom')
+        Template = Model.get('product.template')
+        PurchaseRequest = Model.get('purchase.request')
+        unit, = Uom.find([('name', '=', 'Unit')])
+        template = Template(name='Product', default_uom=unit)
+        template.save()
+        product, = template.products
+        line = PurchaseRequest()
+        line.product = product
+
         for name in ('forecast_quantity', 'available_quantity',
                 'incoming_quantity', 'outgoing_quantity'):
-            self.assertEqual(line.get_product_quantity(name),
-                getattr(product, name))
-
-
-del ModuleTestCase
+            self.assertEqual(getattr(line, name), getattr(product, name))
